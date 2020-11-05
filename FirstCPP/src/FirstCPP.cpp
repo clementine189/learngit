155a87f73a9c2c1b271dfb74c064fe038bf6b7c8
clementine189 <clementine189@163.com>
2020-11-05 15:48:15 +0800

    version 3

---
+++ FirstCPP/src/FirstCPP.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : FirstCPP.cpp
+// Author      : clementine
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "hello git ONCE" << endl;
+	cout << ""<< endl;
+
+	return 0;
+}
